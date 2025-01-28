e95b5879ecef2af3b8c23d345653a2d925e368bc
adevula-DE <devulapally.anudeep@gmail.com>
2025-01-28 18:18:57 -0500

    consumer.py- added DAG task

--- dags/consumer.py
+++ dags/consumer.py
@@ -3,3 +3,14 @@ from airflow.decorators import task
 from datetime import date,datetime
 
 my_file=dataset('/tmp/my_file.txt')
+
+with DAG('consumer',schedule=[my_file],
+         start_date=datetime(2022, 1, 1),
+         catchup=False) as dag:
+    
+    @task
+    def read_dataset():
+        with open(my_file.uri,'r') as f:
+            print(f.read())
+    
+    read_dataset()
