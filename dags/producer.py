cba1135dc2f6c5a89a5b6cc8dae67507bdfef89b
adevula-DE <devulapally.anudeep@gmail.com>
2025-01-28 19:15:22 -0500

    -fixed producer Datset@task

--- dags/producer.py
+++ dags/producer.py
@@ -7,7 +7,7 @@ my_file=Dataset('/tmp/my_file.txt')
 with DAG('producer',start_date=datetime(2025, 1, 1),
          schedule_interval='@daily',catchup=False , tags=['mydags']) as dag:
     
-    @task
+    @task(outlets=[my_file])
     def update_dataset():
         with open(my_file.uri,'a+') as f:
             f.write('producer update')
