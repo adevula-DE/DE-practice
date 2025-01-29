779d5466fda6f26b1735ab92e856914d8eb93e54
adevula-DE <devulapally.anudeep@gmail.com>
2025-01-28 22:24:04 -0500

    added 2nd task for 2nd dataset

--- dags/consumer.py
+++ dags/consumer.py
@@ -3,8 +3,9 @@ from airflow.decorators import task
 from datetime import date,datetime
 
 my_file=Dataset('/tmp/my_file.txt')
+my_file2=Dataset('/tmp/my_file2.txt')
 
-with DAG('consumer',schedule=[my_file],
+with DAG('consumer',schedule=[my_file,my_file2],
          start_date=datetime(2025, 1, 1),
          catchup=False,tags=['mydags']) as dag:
     
@@ -13,4 +14,10 @@ with DAG('consumer',schedule=[my_file],
         with open(my_file.uri,'r') as f:
             print(f.read())
     
+    @task
+    def read_dataset2():
+        with open(my_file2.uri,'r') as f:
+            print(f.read())
+    
     read_dataset()
+    read_dataset2()
--- dags/producer.py
+++ dags/producer.py
@@ -3,6 +3,7 @@ from airflow.decorators import task
 from datetime import date,datetime
 
 my_file=Dataset('/tmp/my_file.txt')
+my_file2=Dataset('/tmp/my_file2.txt')
 
 with DAG('producer',start_date=datetime(2025, 1, 1),
          schedule_interval='@daily',catchup=False , tags=['mydags']) as dag:
@@ -12,4 +13,10 @@ with DAG('producer',start_date=datetime(2025, 1, 1),
         with open(my_file.uri,'a+') as f:
             f.write('producer update')
     
+    @task(outlets=[my_file2])
+    def update_dataset2():
+        with open(my_file2.uri,'a+') as f:
+            f.write('producer update')
+
     update_dataset()
+    update_dataset2()
